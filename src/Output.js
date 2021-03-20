b80aadae7f84d9bf13cff4c01bb40b068a4928cb
AndrejaKardos <andreja.kardos@gmail.com>
2021-03-20 17:36:19 +1100

    Added fizzbuzz logic to Output component

--- src/Output.js
+++ src/Output.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './Output.css'
 
 class Output extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.interpretCount = this.interpretCount.bind(this);
+    }
+
+    interpretCount(count) {
+        if (count % 15 === 0 && count != 0) {
+            return "FIZZBUZZ";
+        } else if (count % 3 === 0 && count != 0) {
+            return "FIZZ";
+        } else if (count % 5 === 0 && count != 0) {
+            return "BUZZ";
+        } else {
+            return count;
+        }
+    }
+
     render() {
         return (
-            <div className="output">{this.props.count}</div>
+            <div className="output">{this.interpretCount(this.props.count)}</div>
         );
     }
 }
