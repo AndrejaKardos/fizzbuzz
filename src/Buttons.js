2b977aa23dcd0368e71b0f1d09a28087f912def5
AndrejaKardos <andreja.kardos@gmail.com>
2021-03-20 16:53:28 +1100

    Set up simple onClick methods for buttons

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,11 +2,25 @@ import React from 'react';
 import './Buttons.css'
 
 class Buttons extends React.Component {
+    constructor(props) {
+        super(props);
+        this.increaseCount = this.increaseCount.bind(this);
+        this.decreaseCount = this.decreaseCount.bind(this);
+    }
+
+    increaseCount() {
+        console.log(this.props.count + 1);
+    }
+
+    decreaseCount() {
+        console.log(this.props.count - 1);
+    }
+
     render() {
         return (
             <div class="buttons">
-                <button class="buttons__btn">+</button>
-                <button class="buttons__btn">-</button>
+                <button class="buttons__btn" onClick={this.increaseCount}>+</button>
+                <button class="buttons__btn" onClick={this.decreaseCount}>-</button>
             </div>
         );
     }
