1a54b81b282438fba553e8cf11d3ad8b9732fad0
AndrejaKardos <andreja.kardos@gmail.com>
2021-03-20 15:26:17 +1100

    Set up main component

---
+++ src/Main.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Main.css";
+
+function Main() {
+    return (
+        <div class="main">
+            <div class="main__wrapper">
+                <div>
+                    +
+                    -
+                </div>
+                <div>
+                    OUTPUT
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Main;
