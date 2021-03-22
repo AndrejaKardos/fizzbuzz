2f0447a6391e1c45731dfdce35b1936cbfd43b0f
AndrejaKardos <andreja.kardos@gmail.com>
2021-03-22 14:35:22 +1100

    Setup additional background styling in Main component

--- src/Main.js
+++ src/Main.js
@@ -30,9 +30,9 @@ class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <div class="main__header">FizzBuzz</div>
+                <div className="main__quarter-circle"></div>
+                <div className="main__header">FizzBuzz</div>
                 <div className="main__wrapper">
-                    
                     <Buttons 
                         count={this.state.count} 
                         newCount={this.newCount}
