7b2980c94b88f0a66b6fcf6a989b11e87c5ebe2e
AndrejaKardos <andreja.kardos@gmail.com>
2021-03-22 18:11:24 +1100

    Added top and bottom border to info question

--- src/Main.js
+++ src/Main.js
@@ -41,7 +41,9 @@ class Main extends React.Component {
                     <Output count={this.state.count} />
                 </div>
                 <div id="main__message" className="hide-message">Output can't be less than 0 or more than 100!</div>
+                <div className="main__border-top"></div>
                 <div className="main__info-question">What is FizzBuzz?</div>
+                <div className="main__border-bottom"></div>
                 <div className="main__info-answer">FizzBuzz is a group word game for children to teach them about division. Players take turns to count incrementally, replacing any number divisible by <b>3</b> with the word <b>Fizz</b>, any number divisible by <b>5</b> with the word <b>Buzz</b>, and any numbers divisible by <b>15</b> with <b>FizzBuzz</b>.</div>
             </div>
         );
