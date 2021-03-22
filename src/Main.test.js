660b650eb161e1f2b17210ce66b04b1fd274a0e3
AndrejaKardos <andreja.kardos@gmail.com>
2021-03-22 16:31:11 +1100

    Unit test: check if numbers divisible by 5 return "Buzz"

--- src/Main.test.js
+++ src/Main.test.js
@@ -16,10 +16,29 @@ test('Check if multiples of 3 (not divisible by 5) return Fizz', () => {
     // we will use our interpretCount function on the array
     // while checking whether any value doesn't equal "Fizz"
 
-    var value = true;
+    let value = true;
 
     for (let j = 0; j < multiplesOfThree.length; j++) {
         if(interpretCount(multiplesOfThree[j]) !== "Fizz") {
+            value = false;
+        } 
+    }
+    
+    expect(value).toBe(true);
+});
+
+test('Check if multiples of 5 (not divisible by 3) return Buzz', () => {
+    const multiplesOfFive = [];
+    for (let i = 1; i <= 20; i++) {
+        if(i*5 % 3 !== 0) {
+            multiplesOfFive.push(i*5);
+        }
+    }
+
+    let value = true;
+
+    for (let j = 0; j < multiplesOfFive.length; j++) {
+        if(interpretCount(multiplesOfFive[j]) !== "Buzz") {
             value = false;
         } 
     }
